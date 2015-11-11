8329d0e0200896df072446e505cb499014e4bf36
Colin Rubbert <colinrubbert@gmail.com>
2015-11-10 18:13:00 -0600

    Refactoring and templating update

---
+++ _rendered/assets/js/functions.js
@@ -0,0 +1,19 @@
+$(function() {
+  smoothScroll(300);
+});
+
+
+// smootScroll function is applied from the document ready function
+function smoothScroll (duration) {
+  $('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, duration);
+    }
+  });
+}
