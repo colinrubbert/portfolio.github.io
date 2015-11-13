01d01dd50d611f69764b7b7288f10e3b7700265a
Colin Rubbert <colinrubbert@gmail.com>
2015-11-12 21:05:21 -0600

    Install fit text for auto adjusting fonts per device

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,7 +5,11 @@ $(function() {
   smoothScroll(300);
   workBelt();
   workLoad();
-  clientStuff()
+  clientStuff();
+
+  $("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
+  $(".biglink").fitText(1.5);
+
 });
 
 
@@ -100,3 +104,37 @@ function clientStuff() {
 
   });
 }
+
+// Fit Text Function - Adjusts font sizes based on device dimensions
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
